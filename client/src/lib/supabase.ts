dc447853c492a80ad35e12db9a1f25c218eac371
Simon Parrott <hallo@newagefotografie.com>
2025-08-22 13:59:44 +0700

    Revert "Update supabase to read from env"
    This reverts commit 45f7e359e5aa1afc9c0f894db28ad411883ac2da.

--- client/src/lib/supabase.ts
+++ client/src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://gtnwccyxwrevfnbkjvzm.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd0bndjY3l4d3JldmZuYmtqdnptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAyNDgwMTgsImV4cCI6MjA2NTgyNDAxOH0.MiOeCq2NCD969D_SXQ1wAlheSvRY5h04cUnV0XNuOrc';
 
 // Supabase configuration initialized
 
