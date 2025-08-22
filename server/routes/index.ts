6b32bcfb4a596400912171bccb76acd5234697d6
Simon Parrott <hallo@newagefotografie.com>
2025-08-22 14:20:28 +0700

    fix

---
+++ server/routes/index.ts
@@ -0,0 +1,6 @@
+// Central export for all route modules
+export * from './calendar';
+export * from './crm-agent';
+export * from './files';
+export * from './gallery-shop';
+export * from './website-wizard';
